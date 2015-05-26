e568898bec43d2e05e78ffb162153eb0c5396bb0
Mosai <mosai@Ultronbook>
2015-05-25 17:01:41 -0700

    Added outputs for each of the completed data structures. More work put into HashTable

--- HashTable/data.cpp
+++ HashTable/data.cpp
@@ -1,2 +1,47 @@
 #include "data.h"
+#include <string.h>
 
+/*
+	Default Constructor.
+	Initializes a blank data object.
+*/
+data::data(){
+	this->word = nullptr;
+	this->int  = NULL;
+}
+
+/*
+	Copy Constructor.
+	Creates a copy of the data object
+	passed to it.
+*/
+data::data(data& data_in){
+	if(&data_in != this){
+		this->set_word(data_in.get_word());
+		this->set_value(data_in.get_value());
+	}
+	return *this;
+}
+
+data::~data(){
+	delete this->word;
+}
+
+char* data::get_word(){
+	return this->word;
+}
+
+int data::get_value(){
+	return this->value;
+}
+
+void data::set_word(char* input){
+	int	len = strlen(input);
+
+	this->word = new char[len + 1];
+	strcpy(input, word);
+}
+
+void data::set_value(int input){
+	this->value = input;
+}
--- HashTable/data.h
+++ HashTable/data.h
@@ -1,15 +1,27 @@
 #ifndef DATA_H
 #define DATA_H
 
+/*
+	Data class will not hold any pointers
+	to other nodes.  All members of the data
+	class will be public, to act as a struct.
+*/
 class data
 {
+public:
 	data();
 	data(data&);
 	~data();
 
-	void	get_word(char*);
-	void	get_value(int);
+	// Accessors
+	char*	get_word();
+	int	get_value();
 
+	// Mutators
+	void	set_word(char*);
+	void    set_value(int);
+
+	// Data variables
 	char*	word;
 	int	value;
 }
